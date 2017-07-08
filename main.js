fe69d1075070418fdd2e4e48290832d7757a3381
jd-erreape <juandediosherrero@gmail.com>
2017-07-08 20:31:01 -0100

    Added observable and refactored conversion to bin

--- main.js
+++ main.js
@@ -1,5 +1,71 @@
-var gpio = require('rpi-gpio');
-var hex2Bool = require('./hex_to_bin');
+const Rx = require('rxjs/Rx');
+const R = require('ramda');
+const gpio = require('rpi-gpio');
+
+// -------- Pad a string with 0 for length 8
+const pad = R.curry((n, string) => (Array(n).join('0') + string).slice(-n));
+const pad8 = pad(8);
+
+// -------- Single char to bin string
+const charToBin = R.compose(
+  pad8,
+  R.invoker(1, 'toString')(2),
+  R.invoker(1, 'charCodeAt')(0)
+);
+
+// -------- String to bin string
+const stringToBin = R.compose(
+  R.join(''),
+  R.map(charToBin),
+  R.split(''),
+);
+
+// -------- Bin string to Bin array
+const stringToBinArray = R.compose(
+  R.map(parseInt),
+  R.split(''),
+  stringToBin
+);
+
+// ------------------------------
+//
+// Observable that will convert the string to bin array as source, will buffer
+// each 4 elements of the stream and will return the buffered packages every
+// n milliseconds
+
+const message = 'ABCDE';
+const stream = Rx.Observable
+  .from(stringToBinArray(message))
+  .bufferCount(8)
+  .zip(
+    Rx.Observable.interval(500),
+    package => package
+  );
+
+stream.subscribe(console.log);
+
+// GPIO Pins we're gonna use
+// they are in the same order as a binary
+
+// const PIN_7 = 14;
+// const PIN_6 = 15;
+// const PIN_5 = 18;
+// const PIN_4 = 23;
+// const PIN_3 = 24;
+// const PIN_2 = 25;
+// const PIN_1 = 8;
+// const PIN_0 = 7;
+//
+// const pinNumbers = [
+//   PIN_7,
+//   PIN_6,
+//   PIN_5,
+//   PIN_4,
+//   PIN_3,
+//   PIN_2,
+//   PIN_1,
+//   PIN_0
+// ];
 
 // Set GPIO pin numbers mode
 gpio.setMode(gpio.MODE_BCM);
@@ -7,7 +73,7 @@ gpio.setMode(gpio.MODE_BCM);
 // GPIO Pins we're gonna use
 // they are in the same order as a binary
 const pinNumbers = [23, 24, 15, 18];
-const boolRepresentation = hex2Bool('D');
+const boolRepresentation = [true, false, true, false];
 
 // Will enable a LED and disable it after 2 seconds
 const writeDigit = () => {
